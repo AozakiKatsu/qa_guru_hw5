b30e103e426bd3643a5cefd0a771eeeb0eb4c226
Irina Luchkova <shtejn@gmail.com>
2024-02-24 12:34:22 +0300

    crtl alt l

--- homework.py
+++ homework.py
@@ -3,10 +3,8 @@ from datetime import time
 
 def test_dark_theme_by_time():
     current_time = time(hour=23)
-
     if time(hour=6) < current_time < time(hour=22):
         is_dark_theme = False
-
     else:
         is_dark_theme = True
     assert is_dark_theme is True
@@ -18,10 +16,8 @@ def test_dark_theme_by_time_and_user_choice():
     if dark_theme_enabled_by_user is None:
         if time(hour=6) < current_time < time(hour=22):
             is_dark_theme = False
-
     elif dark_theme_enabled_by_user:
         is_dark_theme = True
-
     else:
         is_dark_theme = False
     assert is_dark_theme is True
